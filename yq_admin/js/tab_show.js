24bc8b829d5e1bd1016963320daece601fb4ff06
zhangchunjiang <270068451@qq.com>
2018-08-15 19:06:00 +0800

    弈趣后台管理模板

---
+++ yq_admin/js/tab_show.js
@@ -0,0 +1,70 @@
+$(function() {
+	//加载模块
+	layui.use(['form', 'element', 'laypage'],
+		function() {
+			layer = layui.layer;
+			element = layui.element;
+			var laypage = layui.laypage;
+			//执行一个laypage实例,分页模块
+			laypage.render({
+				elem: 'page', //注意，这里的 test1 是 ID，不用加 # 号
+				count: 50 //数据总数，从服务端得到
+			});
+		});
+	$(".test").click(function() {
+		layer.confirm('确认删除？', {
+			btn: ['确定']
+		})
+	});
+
+	$(".test1").click(function() {
+		layer.confirm('确认加入黑名单？', {
+			btn: ['确定'],
+			skin: 'sure'
+		})
+	})
+	$(document).on('click', '.sure .layui-layer-btn0', function() {
+		//确认加入黑名单点击事件
+		layer.msg("已加入黑名单");
+	})
+
+	$(".rizhi").click(function() {
+		layer.confirm('确认删除？', {
+			btn: ['确定'],
+			skin: 'sure_rizhi'
+		})
+	})
+	$(document).on('click', '.sure_rizhi .layui-layer-btn0', function() {
+		//已删除
+		layer.msg("已删除");
+	})
+})
+
+//弹出层组件
+function x_admin_show(title,content,w,h){
+    if (title == null || title == '') {
+        title=false;
+    };
+    if (w == null || w == '') {
+        w = 460;
+    };
+    if (h == null || h == '') {
+        h= 300;
+    };
+    layer.open({
+        area: [w+'px', h +'px'],
+        fix: false, //不固定
+//      maxmin: true,//设置窗口是否可以最小化
+        shadeClose: true,
+        shade:0.4,
+        title: title,
+        btn:['不确定'],
+        skin:'sure_delete',
+        content: content
+    });
+}
+
+$(document).on('click','.sure_delete .layui-layer-btn0',function(){
+	//ajax
+	layer.msg("删除成功！");
+})
--- yq_admin/js/test.js
+++ yq_admin/js/test.js
@@ -1,13 +1,83 @@
-$(function() {
-	layui.use(['form','element'],
-    function() {
-        layer = layui.layer;
-        element = layui.element;
-    });
-
-	$("#test").click(function() {
-		layer.confirm('登陆成功！',{
-			btn:['确定']
-		})
+function insert_btn() {
+	layer.prompt({
+		title: '新增组',
+		btnAlign: 'c',
+		closeBtn: 0
+	}, function(value, index, elem) {
+		//value得到输入的值
+		layer.close(index); //关闭弹出层
+		layer.msg(value);
 	});
-})
+}
+
+function update_btn() {
+	layer.prompt({
+		title: '修改名称',
+		btnAlign: 'c',
+		closeBtn: 0
+	}, function(value, index, elem) {
+		layer.close(index);
+		layer.msg(value);
+	});
+}
+
+function update_btns() {
+	var xitong = $(".xitong").html();
+	layer.prompt({
+		value: xitong,
+		title: '修改名称',
+		btnAlign: 'c',
+		closeBtn: 0
+	}, function(value, index, elem) {
+		layer.close(index);
+		layer.msg(value);
+	});
+}
+
+function delete_btn() {
+	layer.confirm('确认删除？', {
+		btn: ['确定'],
+		skin: 'delete'
+	})
+	$(document).on('click', '.delete', function() {
+		layer.msg('已删除');
+	})
+}
+
+
+
+/*弹出层*/
+/*
+    参数解释：
+    title   标题
+    url     请求的url
+    id      需要操作的数据id
+    w       弹出层宽度（缺省调默认值）
+    h       弹出层高度（缺省调默认值）
+*/
+function y_admin_show(title,url,w,h){
+    if (title == null || title == '') {
+        title=false;
+    };
+    if (url == null || url == '') {
+        url="404.html";
+    };
+    if (w == null || w == '') {
+        w=($(window).width()*0.9);
+    };
+    if (h == null || h == '') {
+        h=($(window).height() - 50);
+    };
+    layer.open({
+        type: 2,
+        area: [w+'px', h +'px'],
+        fix: false, //不固定
+//      maxmin: false,//是否可以放大缩小
+        shadeClose: true,
+        shade:0.4,
+        title: title,
+        content: url
+    });
+}
+
+
